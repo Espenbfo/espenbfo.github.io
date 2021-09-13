b8342d103ba553bd8676010a5321a74a5c02119c
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-09-13 02:07:32 +0200

    Add filter option

---
+++ script.js
@@ -0,0 +1,35 @@
+$(".filter-icon").click(function () {
+    $(".filter-item").toggleClass("no-display")
+    $(".filter-icon").toggleClass("rotated")
+})
+
+let categoryActive = ""
+
+function lower_opacity(name) {
+    $(`.filter-item`).removeClass("active")
+    $(".filter-item").removeClass("not_active")
+    if (categoryActive === name) {
+        $(".project-item").removeClass("lower-opacity")
+        categoryActive = ""
+
+    } else {
+        categoryActive = name;
+        $(`.${name}`).removeClass("lower-opacity")
+        $(`.project-item:not(.${name})`).addClass("lower-opacity")
+        $(`.${name}-toggle`).addClass("active")
+        $(`.filter-item:not(.${name}-toggle)`).addClass("not_active")
+    }
+}
+
+$(".ai-toggle").click(function () {
+    lower_opacity("ai")
+})
+
+$(".website-toggle").click(function () {
+
+    lower_opacity("website")
+})
+
+$(".misc-toggle").click(function () {
+    lower_opacity("misc")
+})
